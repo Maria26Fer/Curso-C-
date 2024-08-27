52da6193b101d7e4a19a6a92948fe1f3636e4ff2
Maria Fernanda Dias Faleiro <103529556+Maria26Fer@users.noreply.github.com>
2024-08-27 09:12:57 -0300

    Add files via upload

---
+++ Capitulo 11/Exercicios Exceções/Course/Course/Entities/Account.cs
@@ -0,0 +1,41 @@
+﻿using Course.Entities.Exception;
+
+namespace Course.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+        public Account() { }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;//Titular
+            Balance = balance;//Saldo
+            WithdrawLimit = withdrawLimit;//Limite de retirada
+        }
+        public void Deposit(double amount) //Quantia
+        {
+            Balance += amount; //Esse metodo vai verificar se é um saldo maior ou igual ao quantia que está armazenada na conta
+        }
+
+        public void Withdraw(double amount)
+            /*Se a quantia for maior que o limite que será retirado
+             o programa vai me retornar um erro dizendo que o valor passou do limite de retirada*/
+        {
+            if (amount > WithdrawLimit) {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            /*Se a quantia for maior que o saldo que tem na conta o programa vai me retornar um erro
+             dizendo que não tem saldo suficiente para realizr o saque*/
+            if (amount > Balance) {
+                throw new DomainException("Not enough balance");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ Capitulo 11/Exercicios Exceções/Course/Course/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Course.Entities.Exception
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message): base(message) 
+        {
+        }
+    }
+}
